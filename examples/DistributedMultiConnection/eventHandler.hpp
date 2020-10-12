3cd4c035ff8f415dfcfc42d402df5262816d8b7a
colson <dud3722000@naver.com>
2020-10-12 22:49:45 +0900

    .

---
+++ examples/DistributedMultiConnection/eventHandler.hpp
@@ -0,0 +1,14 @@
+#ifndef EVENT_HANDLER_H
+#define EVENT_HANDLER_H
+
+
+void handler_error(void);
+void handler_txDone(void);
+void handler_rxTimeStampAvail(void);
+void handler_rxFailed(void);
+void handler_rxTimeOut(void);
+void handler_rxDone(void);
+
+
+
+#endif
